0a379172e1c0cf98b2463b4cb2cfb06e4bed54e0
jmacleod8 <jmacleod8@gmail.com>
2015-12-10 22:47:01 -0400

    removed vulagr comments and stuff from populationsimulation.cpp

--- PopulationSimulator.cpp
+++ PopulationSimulator.cpp
@@ -36,7 +36,6 @@ invalid:
 		return newPop;
 
 	case 2:
-		//aaaaaaaaaaahhhhhhhh
 		
 		std::cout << "Enter population size: ";	
 		std::cin >> popSize;
@@ -74,7 +73,6 @@ invalid:
 		return newPop;
 
 	default:
-		//AAHHHH
 		std::cout<< "Invalid input. Press c to continue.\n";
 		std::cin.clear();
 		std::cin.ignore(80, 'c');
@@ -84,141 +82,6 @@ invalid:
 	}
 }
 
-/*
-int menu2(){
-	int input, iterations;
-	std::cout << "Whattya wanna do with that population loser?: \n";
-	std::cout <<	"	[1] Fitness \n";
-	std::cout <<	"	[2] Random mating (giggity)\n";
-
-
-	std::cin >> input;
-	
-	switch(input){
-	case 1:
-		//sometimes i feel reaaaaally weird
-		return 1;
-	case 2:
-		//My names Dave Fuck
-		iterations = menuGetIterations();
-		for(int i = 0; i<iterations; i++){
-			population
-		}
-
-		return 2;
-	default:
-		//and I'm a Backer.
-
-		std::cout<< "Wrong input type. Expected an integer. \n";
-		return -1;
-	}	
-=======
-//AHHHHHH THIS IS A TEST!
-
-#include "stdafx.h"
-
-Population menu1(){// what population?
-	int input, popSize, numD, numR, numH;
-	double  genoDFreq;
-	Population newPop;
-	//newPop.initialize();
-
-	std::cout << "What kinda population do you want, idiot?: \n";
-	std::cout <<	"	[1] Random population of given size \n";
-	std::cout <<	"	[2] Homozygous population with given frequencies\n";
-	std::cout <<	"	[3] Homozygous population with given numbers of dominant and recessive genotypes \n";
-	std::cout <<	"	[4] Population with given numbers of heterozygous and homozygous genotypes\n";
-
-invalid:
-
-	std::cin >> input;
-
-	switch(input){
-
-	case 1: // population of given size
-
-		std::cout << "Enter population size: \n";
-		std::cin >> popSize;
-		newPop.initialize(popSize);
-		return newPop;
-
-	case 2:
-		//aaaaaaaaaaahhhhhhhh
-		
-		std::cout << "Enter population size: ";	
-		std::cin >> popSize;
-		
-		std::cout << "Enter Dominant genotype frequency (recessive frequency = (1 - freq_D)): "; 
-		std::cin >> genoDFreq;
-
-		newPop.initialize(popSize, genoDFreq);
-		return newPop;
-		//break;
-	case 3:
-
-		std::cout << "Enter number of dominant genotypes: ";	
-		std::cin >> numD;
-
-		std::cout << "Enter number of recessive genotypes: ";	
-		std::cin >> numR;
-
-		newPop.initialize(numD, numR);
-		return newPop;
-		//break;
-
-	case 4:	
-
-		std::cout << "Enter number of dominant homogeneous genotypes: ";	
-		std::cin >> numD;
-
-		std::cout << "Enter number of recessive homogeneous genotypes: ";	
-		std::cin >> numR;
-
-		std::cout << "Enter number of heterogeneous genotypes: ";	
-		std::cin >> numH;
-
-		newPop.initialize(numD, numR, numH);
-		return newPop;
-
-	default:
-		//AAHHHH
-
-		std::cout<< "wrong input type retard!\n expected an integer. \n";
-		goto invalid;
-	}
-}
-
-/*
-int menu2(){
-	int input, iterations;
-	std::cout << "Whattya wanna do with that population loser?: \n";
-	std::cout <<	"	[1] Fitness \n";
-	std::cout <<	"	[2] Random mating (giggity)\n";
-
-
-	std::cin >> input;
-	
-	switch(input){
-	case 1:
-		//sometimes i feel reaaaaally weird
-		return 1;
-	case 2:
-		//My names Dave Fuck
-		iterations = menuGetIterations();
-		for(int i = 0; i<iterations; i++){
-			population
-		}
-
-		return 2;
-	default:
-		//and I'm a Backer.
-
-		std::cout<< "Wrong input type. Expected an integer. \n";
-		return -1;
-	}	
->>>>>>> origin/master
-}
-*/
 int menuGetIterations(){
 	int i;
 	std::cout<< "How many generations: ";
@@ -230,28 +93,6 @@ int menuGetIterations(){
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	/*
-	for(int i = 0; i < 10; i++)
-    {
-	    gene test;
-	    test.initialize();
-	    test.printGene(); 
-    }
-	
-	
-	Population pop;
-	pop.initialize(20);
-	double p = pop.getFreqP();
-	std::cout << std::fixed << p;
-	pop.printStats(p);
-	pop = pop.newGeneration();
-	pop.printStats(p);
-	std::cout << "butts";
-	//double initFreqP = population.getFreqP();
-	
-
-//	int input;
-	*/
 
 
 	Population population = menu1();
@@ -269,7 +110,6 @@ invalid2:
 	switch(input){
 	case 1:
 		double fitAA, fitAa, fitaa;
-		//sometimes i feel reaaaaally weird
 		std::cout << "Enter the fitness for genotype AA: ";
 		std::cin >> fitAA;
 		std::cout << "Enter the fitness for genotype Aa: ";
@@ -294,7 +134,6 @@ invalid2:
 		break;
 		//return 1;
 	case 2:
-		//My names Dave Fuck
 		iterations = menuGetIterations();
 		
 
@@ -314,7 +153,6 @@ invalid2:
 		break;
 		//return 2;
 	default:
-		//and I'm a Backer.
 		std::cout<< "Invalid input. Press c to continue.\n";
 		std::cin.clear();
 		std::cin.ignore(80, 'c');
@@ -322,15 +160,6 @@ invalid2:
 		goto invalid2;
 		//return -1;
 	}	
-	//input = menu2();
-	//while(input == -1){input = menu2();}
-
-	
-	//population.printStats(.5);
-
-	
-
-	//for(int i = 0; i < 15; i++){graph.addFreq((double)(i)/15);}
 	std::cout<< "\nWould you like a graph of the first 80 generations? (y/n): ";
 	char cinput;
 invalid3:
